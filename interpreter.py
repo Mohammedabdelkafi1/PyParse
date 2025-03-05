837e8274156410501a1a3704466de9fdc048810c
Mohammedabdelkafi1 <kawruulife@gmail.com>
2025-03-05 19:39:29 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -5,8 +5,11 @@ letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 class Lexer:
     def __init__(self):
         self.pos = -1
+        self.curr_char= None
         self.tokens = []
     def advance(self):
         self.pos+=1
         self.curr_char = src[self.pos]
-        
+    def tokenize(self):
+        if curr_char=="+":
+            self.tokens.append("")
